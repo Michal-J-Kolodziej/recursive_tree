87698874dce00455c71d96bf991dff4ad9c166a2
Michał Kołodziej <asezrik@gmail.com>
2019-02-10 11:25:51 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+c.fillStyle = '#aaa';
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 const handleBrowserResize = () => {
     index = 0;
@@ -24,27 +26,39 @@ let startLineSize = canvas.height * 0.17;
 
 const angle = (Math.PI / 6);
 let index = 0;
-const q = 0.77; //Don't even try 0.83 or higher
+const q = 0.75; //Don't even try 0.83 or higher
 
 const drawLine = (x, y, s, a = 0) => {
     index++;
     console.log(index);
     const nextX = 0;
     const nextY = -s;
+    c.beginPath();
     c.translate(x, y);
     c.rotate(a);
     c.moveTo(0, 0);
     c.lineTo(nextX, nextY);
+    c.strokeStyle = 'white';
+    if (s < 15) {
+        c.strokeStyle = 'lime';
+    }
+    c.lineWidth = 2;
     c.stroke();
+    c.closePath();
 
 
     if (s > 5) {
-        c.save();
-        drawLine(nextX, nextY, s * q, -angle);
-        c.restore();
+        let randQ = (Math.random() * ((q + 0.05) - q) + q).toFixed(2);
+        let randAngle = Math.PI / (Math.random() * (10 - 5) + 5).toFixed(0);
 
         c.save();
-        drawLine(nextX, nextY, s * q, angle);
+        drawLine(nextX, nextY, s * randQ, -randAngle);
+        c.restore();
+
+        // randQ = (Math.random() * ((q + 0.05) - q) + q).toFixed(2);
+
+        c.save();
+        drawLine(nextX, nextY, s * randQ, randAngle);
         c.restore();
     }
 }
